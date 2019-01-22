1003754e5e822783096e42ac535647430acb6034
kacper91 <kacper.orlowski.1991@gmail.com>
2019-01-22 16:06:50 +0100

    6.4.

--- kodilla-testing/src/main/java/com/kodilla/testing/shape/App.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/shape/App.java
@@ -1,17 +1,21 @@
 package com.kodilla.testing.shape;
-import com.kodilla.testing.shape.ShapeCollector;
-import java.util.ArrayList;
-import java.util.List;
+
 
 public class App {
     public static void main(String[] args) {
 
-        List<Shape> shapeCollection = new ArrayList<>();
-
-        Circle circleOne = new Circle("nameForNewCircle",30.5);
+        Circle circleOne = new Circle("nameForNewCircle", 30.5);
+        Triangle triangleOne = new Triangle("nameForNewTriangle", 40.5);
+        Square squareOne = new Square("nameForNewSquare", 66);
         ShapeCollector newShape = new ShapeCollector(circleOne);
+
         newShape.addFigure(circleOne);
-        System.out.println(shapeCollection.size());
+        newShape.addFigure(triangleOne);
+        newShape.addFigure(squareOne);
+        newShape.removeFigure(circleOne);
+        newShape.getFigure(0);
+        newShape.showFigures();
+
 
     }
 }
--- kodilla-testing/src/main/java/com/kodilla/testing/shape/ShapeCollector.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/shape/ShapeCollector.java
@@ -9,31 +9,34 @@ interface Shape {
 
 public class ShapeCollector {
 
+
+
     public ShapeCollector(Shape shape) {
 
     }
-
     List<Shape> shapeCollection = new ArrayList<>();
+
 
     public void addFigure(Shape shape) {
 
         shapeCollection.add(shape);
+        System.out.println("add: " + shapeCollection.size());
     }
 
     public void removeFigure(Shape shape) {
         shapeCollection.remove(shape);
+        System.out.println("remove: " + shapeCollection.size());
     }
 
     public void getFigure(int n) {
-        shapeCollection.get(n);
+
+        System.out.println(shapeCollection.get(n).getShapeName() + " " + shapeCollection.get(n).getField());
     }
 
     public void showFigures() {
         for (Shape allFigures : shapeCollection) {
-            System.out.println(allFigures);
+            System.out.println("all figures: " + allFigures.getShapeName()+ " " + allFigures.getField());
 
         }
     }
-
-
 }
