1445cba6060a91a3a82e078485ce1b8efbf0bbb5
kacper91 <kacper.orlowski.1991@gmail.com>
2019-07-22 19:12:44 +0200

    First

--- kodilla-spring/src/main/java/com/kodilla/spring/forum/ForumUser.java
+++ kodilla-spring/src/main/java/com/kodilla/spring/forum/ForumUser.java
@@ -1,20 +1,4 @@
 package com.kodilla.spring.forum;
 
-import org.springframework.stereotype.Component;
-
-@Component
 public class ForumUser {
-    private String username;
-
-    public ForumUser(String username) {
-        username = "John Smith";
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
 }
