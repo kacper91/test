e2c41f389d3783d6ed738580d899523fe96db531
kacper91 <kacper.orlowski.1991@gmail.com>
2019-07-23 11:41:04 +0200

    Test

--- kodilla-spring/src/main/java/com/kodilla/spring/SpringRunner.java
+++ kodilla-spring/src/main/java/com/kodilla/spring/SpringRunner.java
@@ -3,6 +3,7 @@ package com.kodilla.spring;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class SpringRunner {
 
     public static void main(String[] args) {
--- kodilla-spring/src/main/java/com/kodilla/spring/shape/Circle.java
+++ kodilla-spring/src/main/java/com/kodilla/spring/shape/Circle.java
@@ -7,7 +7,7 @@ public class Circle implements Shape {
 
     @Override
     public String getShapeName() {
-        return "This is a circle";
+        return "This is a circle.";
 
     }
 
