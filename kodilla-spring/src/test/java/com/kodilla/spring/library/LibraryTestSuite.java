a4cf99a05669e39e9888e6fabc33b9ac4f09f5d3
kacper91 <kacper.orlowski.1991@gmail.com>
2019-07-24 19:59:29 +0200

    spring

---
+++ kodilla-spring/src/test/java/com/kodilla/spring/library/LibraryTestSuite.java
@@ -0,0 +1,42 @@
+package com.kodilla.spring.library;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+
+public class LibraryTestSuite {
+
+    @Test
+    public void testLoadFroDb() {
+        //Given
+        ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring");
+        Library library= context.getBean(Library.class);
+
+        //When
+        library.loadFromDb();
+
+        //Then
+        //empty
+    }
+
+    @Test
+    public void testSaveToDb(){
+
+        //Given
+        ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring");
+        Library library = context.getBean(Library.class);
+
+        //When
+        library.savteToDb();
+
+        //Then
+        //empty
+    }
+
+}
