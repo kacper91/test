547cb1149e034a23370a9e8b6c4c03178e591b40
kacper91 <kacper.orlowski.1991@gmail.com>
2019-08-03 12:44:07 +0200

    spring2

--- kodilla-spring/src/test/java/com/kodilla/spring/portfolio/BoardTestSuite.java
+++ kodilla-spring/src/test/java/com/kodilla/spring/portfolio/BoardTestSuite.java
@@ -8,24 +8,26 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import java.util.List;
+
 @SpringBootTest
 public class BoardTestSuite {
 
     @Test
-    public void testTaskAdd(){
+    public void testTaskAdd() {
 
         //Given
         ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring.portfolio");
         Board board = context.getBean(Board.class);
+
+        //When
         board.getToDoList().getTasks().add("zad1");
+        board.getToDoList().getTasks().add("zad2");
 
-
-
-        //WhenThen
-        Assert.assertEquals(1, board.getToDoList().getTasks().size());
+        //Then
+        Assert.assertEquals(2, board.getToDoList().getTasks().size());
         Assert.assertEquals(0, board.getDoneList().getTasks().size());
+        Assert.assertEquals(0, board.getInProgressList().getTasks().size());
 
     }
-
 
 }
