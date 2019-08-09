2fbb77dd18d3aa6e144a6c292e5f4af30aa72ae2
kacper91 <kacper.orlowski.1991@gmail.com>
2019-08-09 11:49:37 +0200

    db

--- kodilla-patterns/src/main/java/com/kodilla/patterns/strategy/Customer.java
+++ kodilla-patterns/src/main/java/com/kodilla/patterns/strategy/Customer.java
@@ -2,10 +2,11 @@ package com.kodilla.patterns.strategy;
 
 public class Customer {
     final private String name;
-    protected BuyPredictor buyPredictor;
+    protected  BuyPredictor buyPredictor;
 
     public Customer(String name){
         this.name=name;
+
     }
     public String getName(){
         return name;
